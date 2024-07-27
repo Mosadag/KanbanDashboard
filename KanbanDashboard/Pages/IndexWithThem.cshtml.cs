91b2c6622764e71cc06845d01ffa07bf97ba344c
Mosadag <mosadag@gmail.com>
2024-07-27 21:45:37 +0300

    changes

---
+++ KanbanDashboard/Pages/IndexWithThem.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KanbanDashboard.Pages
+{
+    public class IndexWithThemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
